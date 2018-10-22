cae09d5e9757e1a7e9765006f4c3273ba01439dc
Milad <miladfm@gmail.com>
2018-10-22 22:50:56 +0200

    create github page

--- src/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ngx-pretty-checkbox
- */
-
-// export * from './lib/ngx-pretty-checkbox.service';
-export * from './lib/components/ngx-pretty-checkbox.component';
-export * from './lib/ngx-pretty-checkbox.module';
-export * from './lib/model/interfaces';
